14fe9954057bfb727651ea87c3a021c7c4a5ac41
“Vlad <vlad.atsman@gmail.com>
2020-04-30 13:20:48 +0300

    clean something

---
+++ src/main/java/com/university/algorithms/Lab3.java
@@ -0,0 +1,4 @@
+package com.university.algorithms;
+
+public class Lab3 {
+}
--- src/main/java/com/university/examplesoferrors/MemoryTest.java
+++ src/main/java/com/university/examplesoferrors/MemoryTest.java
@@ -1,22 +1,21 @@
 package com.university.examplesoferrors;
 
-public class MemoryTest extends ClassLoader {
+import javassist.ClassPool;
+
+public class MemoryTest {
     private static Object v = new Object();
+    static ClassPool classPool = ClassPool.getDefault();
 
-    MemoryTest() {
-        MemoryTest memoryTest = new MemoryTest();
-    }
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         Thread.sleep(15000);
-        recursion();
+        notObviousRecursion();
     }
 
-    public static void withoutHeapSpace() {
+    public static void heapErrorWithoutHeap() {
         float[] outOfMemory = new float[999_999_999];
     }
 
-    public static void bigObjects() {
+    public static void heapErrorWithObjectsWithoutCollections() {
         while (true) {
             Object c = v;
             v = new Object() {
@@ -25,7 +24,11 @@ public class MemoryTest extends ClassLoader {
         }
     }
 
-    public void metaspace() {
+    public static void metaSpaceError() throws Exception {
+        for (int i = 0; i < 1000000000; i++) {
+            Class c = classPool.makeClass("com.university.examplesoferrors.MemoryTest" + i).toClass();
+            System.out.println(c.getName());
+        }
     }
 
 
@@ -33,7 +36,35 @@ public class MemoryTest extends ClassLoader {
         recursion();
     }
 
-    public static void implicitRecursion() {
-        MemoryTest memoryTest = new MemoryTest();
+    public static void notObviousRecursion() {
+        Object1 object1 = new Object1();
+        Object2 object2 = new Object2();
+
+        object1.t = object2;
+        object2.t = object1;
+        String str = object1.toString();
     }
 }
+
+class Object1
+{
+    Long i;
+    Object2 t;
+
+    @Override
+    public String toString() {
+        return t.toString() + i;
+    }
+}
+
+class Object2
+{
+    Long i;
+    Object1 t;
+
+    @Override
+    public String toString() {
+        return t.toString() + i;
+    }
+}
+
