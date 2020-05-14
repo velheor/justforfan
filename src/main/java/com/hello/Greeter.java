61d3044b76d197c61eb8b904fff02d8a58a1d7dc
“Vlad <vlad.atsman@gmail.com>
2020-05-14 14:58:42 +0300

    lab3

--- src/main/java/com/hello/Greeter.java
+++
@@ -1,7 +0,0 @@
-package com.hello;
-
-public class Greeter {
-    public String sayHello() {
-        return "Hello world!";
-    }
-}
--- src/main/java/com/university/algorithms/Lab3.java
+++ src/main/java/com/university/algorithms/Lab3.java
@@ -1,26 +1,48 @@
 package com.university.algorithms;
 
+import java.util.Arrays;
+
 public class Lab3 {
     public static void main(String[] argv) {
-        int[] buyer = {1, 5, 20};
-        int[] seller = {100};
-        int[] allmoney = unionTwoMassives(buyer, seller);
+        Integer[] buyer = {5, 1, 2};
+        Integer[] seller = {4};
+        int price = 8;
+        int amountOfMoney = sumOfMoney(unionTwoMassives(buyer, seller));
+        Integer[] canBuy = new Integer[amountOfMoney];
 
-        insertionSort(allmoney);
-
-        int s = 0;
-        int i = 1;
-
-        while (i <= buyer.length && allmoney[i] <= (s + 1)) {
-            s = s + allmoney[i];
-            i = i + 1;
+        Arrays.fill(canBuy, 0);
+        canBuy[0] = 1;
+        for (Integer coin : buyer) {
+            for (int i = amountOfMoney-1; i > 0; i--) {
+                if ((canBuy[i] == 1) && ((i + coin) <= amountOfMoney)) {
+                    canBuy[i + coin] = 1;
+                }
+            }
+            canBuy[coin] = 1;
         }
-
-        System.out.println(s);
+        for (Integer coin : seller) {
+            for (int i = 0; i < amountOfMoney; i++) {
+                if (canBuy[i] == 1 && coin <= i) {
+                    canBuy[i - coin] = 1;
+                }
+            }
+        }
+        if (canBuy[price] == 1) {
+            System.out.println("Yes");
+        } else
+            System.out.println("No");
     }
 
-    public static int[] unionTwoMassives(int[] a, int[] b) {
-        int[] c = new int[a.length + b.length];
+    public static int sumOfMoney(Integer[] allMoney) {
+        int sum = 0;
+        for (int num : allMoney) {
+            sum = sum + num;
+        }
+        return sum;
+    }
+
+    public static Integer[] unionTwoMassives(Integer[] a, Integer[] b) {
+        Integer[] c = new Integer[a.length + b.length];
 
         int count = 0;
 
@@ -35,7 +57,7 @@ public class Lab3 {
         return c;
     }
 
-    public static void insertionSort(int[] a) {
+    public static void insertionSort(Integer[] a) {
         int N = a.length;
         for (int i = 1; i < N; i++) {
             for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
@@ -48,9 +70,10 @@ public class Lab3 {
         return v.compareTo(w) < 0;
     }
 
-    private static void exch(int[] a, int i, int j) {
+    private static void exch(Integer[] a, int i, int j) {
         int t = a[i];
         a[i] = a[j];
         a[j] = t;
     }
 }
+
