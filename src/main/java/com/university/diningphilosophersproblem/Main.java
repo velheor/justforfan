51abf8e3d63f6e2dc6c29c17f309919f51212947
“Vlad <vlad.atsman@gmail.com>
2020-05-27 12:55:06 +0300

    ку

--- src/main/java/com/university/diningphilosophersproblem/Main.java
+++ src/main/java/com/university/diningphilosophersproblem/Main.java
@@ -1,30 +1,44 @@
 package com.university.diningphilosophersproblem;
 
+
 class Philosopher implements Runnable {
     private final Object leftFork;
     private final Object rightFork;
+    private final int leftForkNum;
+    private final int rightForkNum;
+    private int foodLeft = 12;
 
-    public Philosopher(Object leftFork, Object rightFork) {
+    public Philosopher(Object leftFork, Object rightFork, int leftForkNum, int rightForkNum) {
         this.leftFork = leftFork;
         this.rightFork = rightFork;
+        this.leftForkNum = leftForkNum;
+        this.rightForkNum = rightForkNum;
     }
 
     public void doAction(String action) throws InterruptedException {
         System.out.println(Thread.currentThread().getName() + " " + action);
-        Thread.sleep((int) (Math.random() * 100));
+        Thread.sleep(100);
     }
 
     @Override
     public void run() {
         try {
-            doAction(": Thinking");
-            synchronized (leftFork) {
-                doAction(" : Picked up left fork");
-                synchronized (rightFork) {
-                    doAction(" : Picked up right fork and started eating");
-                    doAction(" : Put down right fork");
+            while (foodLeft > 0) {
+                doAction(": Thinking");
+                synchronized (leftFork) {
+                    doAction("Food left : " + this.foodLeft + " : Picked up left fork" + leftForkNum);
+                    synchronized (rightFork) {
+                        doAction(
+                                "Food left: " + this.foodLeft +
+                                        " : Picked up right fork " + rightForkNum + " and started eating");
+                        doAction(
+                                "Food left : " + this.foodLeft +
+                                        " : Put down right fork");
+                        this.foodLeft -= 1;
+                    }
+                    doAction("Food left : " + this.foodLeft +
+                            " : Put down left fork and start thinking");
                 }
-                doAction(" : Put down left fork and start thinking");
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -34,8 +48,8 @@ class Philosopher implements Runnable {
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Philosopher[] philosopher = new Philosopher[5];
-        Object[] fork = new Object[5];
+        Philosopher[] philosopher = new Philosopher[6];
+        Object[] fork = new Object[6];
 
         for (int i = 0; i < philosopher.length; i++) {
             fork[i] = new Object();
@@ -43,16 +57,16 @@ public class Main {
 
         for (int i = 0; i < philosopher.length; i++) {
             Object leftFork = fork[i];
-            Object rightFork = fork[(i + 1) % 5];
+            Object rightFork = fork[(i + 1) % 6];
 
-            //use this if condition to avoid dead lock
             if (i == philosopher.length - 1) {
-                philosopher[i] = new Philosopher(leftFork, rightFork);
+                philosopher[i] = new Philosopher(leftFork, rightFork, i, (i + 1) % 6);
             } else {
-                philosopher[i] = new Philosopher(rightFork, leftFork);
+                philosopher[i] = new Philosopher(rightFork, leftFork, (i + 1) % 6, i);
             }
             Thread t = new Thread(philosopher[i], " Philosopher " + (i + 1));
             t.start();
         }
+
     }
 }
