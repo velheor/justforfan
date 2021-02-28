b09bb524200fe97112da508d8114893f85928db0
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-02-28 16:45:28 +0300

    bubble sum

---
+++ codewars/easy/src/test/java/com/algo/TwoSumTest.java
@@ -0,0 +1,13 @@
+package com.algo;
+
+import static java.util.Objects.deepEquals;
+
+import org.junit.jupiter.api.Test;
+
+class TwoSumTest {
+
+  @Test
+  void solution() {
+    deepEquals(new int[]{0, 2}, TwoSum.solution(new int[]{1, 2, 3}, 4));
+  }
+}
