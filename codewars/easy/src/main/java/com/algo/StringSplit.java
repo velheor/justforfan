a5a96c7fe18377f486e6b4ebcc644715b99ffd9f
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-02-28 16:09:46 +0300

    first look

---
+++ codewars/easy/src/main/java/com/algo/StringSplit.java
@@ -0,0 +1,25 @@
+package com.algo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSplit {
+
+  public static String[] solution(String s) {
+    List<String> result = new ArrayList<>();
+    StringBuilder split = new StringBuilder();
+
+    for (int i = 0; i < s.toCharArray().length; i++) {
+      split.append(s.toCharArray()[i]);
+      if (s.toCharArray().length % 2 != 0 && i == (s.toCharArray().length - 1)) {
+        split.append("_");
+      }
+      if (split.length() == 2) {
+        result.add(split.toString());
+        split = new StringBuilder();
+      }
+    }
+
+    return result.toArray(String[]::new);
+  }
+}
---
+++ codewars/easy/src/test/java/com/algo/StringSplitTest.java
@@ -0,0 +1,20 @@
+package com.algo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class StringSplitTest {
+
+  @Test
+  void solution() {
+    String b = "abcde";
+    String s = "abcdef";
+    String s1 = "HelloWorld";
+    assertEquals("[ab, cd, ef]", Arrays.toString(StringSplit.solution(s)));
+    assertEquals("[He, ll, oW, or, ld]", Arrays.toString(StringSplit.solution(s1)));
+    assertEquals("[ab, cd, e_]", Arrays.toString(StringSplit.solution(b)));
+
+  }
+}
