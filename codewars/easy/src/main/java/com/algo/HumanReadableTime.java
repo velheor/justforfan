4df9e779c4d0dbb91c8e10c242b0ad0b5ae12924
Uladzislau Atsman <vlad.atsman@gmail.com>
2021-02-28 20:04:39 +0300

    recursive

---
+++ codewars/easy/src/main/java/com/algo/HumanReadableTime.java
@@ -0,0 +1,8 @@
+package com.algo;
+
+public class HumanReadableTime {
+
+  public static String makeReadable(int seconds) {
+    return String.format("%02d:%02d:%02d", seconds / 3600, (seconds / 60) % 60, seconds % 60);
+  }
+}
